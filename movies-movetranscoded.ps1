8ddd5b2534743b5f4ef5cb4e316235ac00f52e77
Bradley Herbst <bordwalk2000@gmail.com>
2021-10-13 23:15:22 -0500

    inital commit

---
+++ movies-movetranscoded.ps1
@@ -0,0 +1,42 @@
+param(
+    [Parameter(
+        Mandatory = $true,
+        Position = 0,
+        HelpMessage = "Path to the location of transcoded movie folders ready to be moved."
+    )]
+    [ValidateNotNullOrEmpty()]
+    [string] $TranscodedFiles,
+
+    [Parameter(
+        Mandatory = $false,
+        HelpMessage = "Skip folders that have LastWriteTime set to less than this many hours ago."
+    )]
+    [string] $HoursLastWrite = 6
+)
+
+$MovieDirectoryLocations = Get-Volume
+| Where-Object { $_.DriveType -eq 'Fixed' -and (Test-Path "$($_.DriveLetter):\Movies") }
+| Select-Object @{label = "MovieDirectories"; Expression = { "$($_.DriveLetter):\Movies" } }
+
+$MovieFolders = Get-ChildItem -Path $MovieDirectoryLocations.MovieDirectories
+| Select-Object Name, FullName,
+@{label = "minascii"; Expression = { [int[]][char[]]($_.Name).replace('-', '')[0] } },
+@{label = "maxascii"; Expression = { [int[]][char[]]($_.Name).replace('-', '')[1] } }
+| Sort-Object Name
+
+$Transcoded = Get-ChildItem $TranscodedFiles -Directory
+| Where-Object { $_.GetFiles().Count -ne 0 -and $_.LastWriteTime -lt (Get-Date).AddHours(-$HoursLastWrite) }
+
+foreach ($Movie in $Transcoded) {
+    #Remove Empty Folders
+    Get-ChildItem -Path $Movie -recurse
+    | Where-Object { $_.PSIsContainer -and @(Get-ChildItem -Lit $_.Fullname -r | Where-Object { !$_.PSIsContainer }).Length -eq 0 }
+    | Remove-Item -Recurse
+
+    #Move Movie Folders
+    foreach ($Directory in $MovieFolders) {
+        if (([int]$Movie.Name[0] -ge $Directory.minascii) -and ([int]$Movie.Name[0] -le $Directory.maxascii)) {
+            Move-Item -Path $Movie.FullName -Destination $Directory.FullName -ErrorAction Continue
+        }
+    }
+}
